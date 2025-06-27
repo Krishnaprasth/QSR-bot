d3a90cf326823e5bd0d6e26f3ffc0fc47b807bd0
Krishnaprasth <ca.krishnaprasath@gmail.com>
2025-06-27 17:48:57 +0530

    Delete counterfactuals.py

--- counterfactuals.py
+++
@@ -1,6 +0,0 @@
-
-def simulate_scenario(df, cost_head, cap_value):
-    df_sim = df.copy()
-    affected = df_sim['Metric'] == cost_head
-    df_sim.loc[affected, 'Amount'] = df_sim.loc[affected, 'Amount'].clip(upper=cap_value)
-    return df_sim
